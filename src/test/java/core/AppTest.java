7ed07d56833692449cda33d8c639b83bcb6fec8d
SvetlanaBojenkova <bojenkova@outlook.com>
2015-02-05 00:23:48 -0800

    Initial commit

--- src/test/java/core/AppTest.java
+++ src/test/java/core/AppTest.java
@@ -12,100 +12,106 @@ import org.junit.Test;
 public class AppTest {
 
 	@BeforeClass
-	public static void setUpBeforeClass() throws Exception {
-	System.out.println("Beforeclass method will be executed before first test method starts");
+	public static void BeforeClass() throws Exception {
+	// System.out.println("BeforeClass method will be executed before first test method starts"); 
 	
 
 	}
 
 	@AfterClass
-	public static void tearDownAfterClass() throws Exception {
-	System.out.println("AfterClass method will be executed after last test method complited");	
+	public static void AfterClass() throws Exception {
+	// System.out.println("AfterClass method will be executed after last test method complited");	
 	}
 	
 
 	@Before
-	public void setUp() throws Exception {
-	System.out.println("Before method will execute before every test method");
+	public void Before() throws Exception {
+	// System.out.println("Before method will execute before every test method");
 	}
 	
 
 	@After
-	public void tearDown() throws Exception {
-	System.out.println("After method will execute after every test method");
+	public void After() throws Exception {
+	// System.out.println("After method will execute after every test method");
 	}
 	
 
 	@Test
 	public void test_01_assertEquals_Positive() {
-		System.out.println("Test _01_assertEquals_Positive");
-		assertEquals("Test case fail","hi","hi");
-		
-		
-	}
-	@Test
-	public void test_02_assertEquals_Negative() {
-		System.out.println("Test _02_assertEquals_Negative");
-		assertEquals("Test case fail","hi","bye");
+		// System.out.println("Test _01_assertEquals_Positive");
+		assertEquals("String not the same","Testing JUnit 4", App.s);
 		
 		
 	}
 	
+	@Test
+	public void test_02_assertEquals_Negative() {
+		// System.out.println("Test _02_assertEquals_Negative");
+		assertEquals("String not the same","Testing TestNG 6", App.s);
+		
+	}
+	
+	
 	@Ignore
 	@Test
 	public void test_03_assertEquals_Ignored() {
-		System.out.println("Test _03_assertEquals_Ignored");
-		assertEquals("Test case fail","la","la");
+		// System.out.println("Test _03_assertEquals_Ignored");
+		assertEquals("String not the same","Testing JUnit 3", App.s);
+		
 		
 		
 	}
 	@Test
 	public void test_04_assertSame_Positive() {
-		System.out.println("Test _04_assertSame_Positive");
-		assertSame("Test case fail",12,12);
+		// System.out.println("Test _04_assertSame_Positive");
+		assertSame("Integer not the same", 55, App.i);
 		
 		
 	}
+	
 	@Test
 	public void test_05_assertSame_Negative() {
-		System.out.println("Test _05_assertSame_Negative");
-		assertSame("Test case fail",12,13);
+		// System.out.println("Test _05_assertSame_Negative");
+		assertSame("Integer not the same", 25, App.i);
+		
 		
 		
 	}
 	@Ignore
 	@Test
 	public void test_06_assertSame_Ignored() {
-		System.out.println("Test _06_assertSame_Ignored");
-		assertSame("Test case fail",150,300);
+		// System.out.println("Test _06_assertSame_Ignored");
+		assertSame("Integer not the same", 100, App.i);
 		
 		
 	}
 	@Test
 	public void test_07_assertFalse_Positive() {
-		System.out.println("Test _07_assertFalse_Positive");
-		assertFalse("Test case fail",false);
+		// System.out.println("Test _07_assertFalse_Positive");
+		assertFalse("Boolean should be faulse", App.f);
 		
 		
 	}
+	
 	@Test
 	public void test_08_assertFalse_Negative(){
-		System.out.println("Test _08_assertFalse_Negative");
-		assertFalse("Test case fail",true);
+		// System.out.println("Test _08_assertFalse_Negative");
+		assertFalse("Boolean should be faulse", App.t);
 		
 		
 	}
 	@Test
 	public void test_09_assertTrue_Positive() {
-		System.out.println("Test _09_assertTrue_Positive");
-		assertTrue("Test case fail",true);
+		// System.out.println("Test _09_assertTrue_Positive");
+		assertTrue("Boolean should be true", App.t);
 		
 		
 	}
+	
 	@Test
 	public void test_10_assertTrue_Negative() {
-		System.out.println("Test _10_assertTrue_Negative");
-		assertTrue("Test case fail",false);
+		//System.out.println("Test _10_assertTrue_Negative");
+		assertTrue("Boolean should be true", App.f);
 		
 		
 	}
